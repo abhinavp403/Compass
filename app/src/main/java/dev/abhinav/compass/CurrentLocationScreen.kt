c557f42daa917433785087624c1fa0aa32000278
abhinavp403 <abhinavp403@gmail.com>
2024-02-17 17:51:45 -0500

    Display user's city and country

--- app/src/main/java/dev/abhinav/compass/CurrentLocationScreen.kt
+++ app/src/main/java/dev/abhinav/compass/CurrentLocationScreen.kt
@@ -2,48 +2,63 @@ package dev.abhinav.compass
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.location.Geocoder
+import android.os.Looper
+import android.util.Log
 import android.view.animation.Animation
 import android.view.animation.RotateAnimation
-import androidx.annotation.RequiresPermission
-import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.Priority
-import com.google.android.gms.tasks.CancellationTokenSource
 import dev.abhinav.compass.ui.theme.CompassTheme
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
+import java.io.IOException
+import java.util.Locale
+import java.util.concurrent.TimeUnit
+
+const val REQUEST_LOCATION_PERMISSION: Int = 300
+//A callback for receiving notifications from the FusedLocationProviderClient.
+lateinit var locationCallback: LocationCallback
+//The main entry point for interacting with the Fused Location Provider
+lateinit var locationProvider: FusedLocationProviderClient
 
 @Composable
 fun CompassScreen() {
@@ -66,6 +81,8 @@ fun CompassScreen() {
         }
     }
 
+    val currentUserLocation = getUserLocation(LocalContext.current)
+
     Column(
         Modifier
             .fillMaxSize()
@@ -75,13 +92,29 @@ fun CompassScreen() {
         Image(
             painter = painterResource(id = R.drawable.compass_image),
             contentDescription = "compass",
-            modifier = Modifier.wrapContentHeight().fillMaxWidth().rotate(sensorEventListener.currentDegree),
+            modifier = Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .rotate(sensorEventListener.currentDegree),
             contentScale = ContentScale.Crop
         )
 
         Spacer(modifier = Modifier.height(30.dp))
 
         SensorDataDisplay(sensorEventListener.currentDegree)
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Row {
+            Icon(
+                imageVector = Icons.Filled.LocationOn,
+                contentDescription = "location icon",
+                tint = Color.Red
+            )
+            Text(
+                text = getReadableLocation(currentUserLocation.latitude, currentUserLocation.longitude, LocalContext.current),
+            )
+        }
     }
 }
 
@@ -118,71 +151,81 @@ class SensorEventListenerImpl : SensorEventListener {
 
 @SuppressLint("MissingPermission")
 @Composable
-fun CurrentLocationScreen() {
-    val permissions = listOf(
-        Manifest.permission.ACCESS_COARSE_LOCATION,
-        Manifest.permission.ACCESS_FINE_LOCATION,
-    )
-    PermissionBox(
-        permissions = permissions,
-        requiredPermissions = listOf(permissions.first()),
-        onGranted = {
-            CurrentLocationContent(
-                usePreciseLocation = it.contains(Manifest.permission.ACCESS_FINE_LOCATION),
-            )
-        },
-    )
+fun getUserLocation(context: Context): LatAndLong {
+    locationProvider = LocationServices.getFusedLocationProviderClient(context)
+    var currentUserLocation by remember { mutableStateOf(LatAndLong()) }
+
+    DisposableEffect(key1 = locationProvider) {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(result: LocationResult) {
+                locationProvider.lastLocation
+                    .addOnSuccessListener { location ->
+                        location?.let {
+                            val lat = location.latitude
+                            val long = location.longitude
+                            // Update data class with location data
+                            currentUserLocation = LatAndLong(latitude = lat, longitude = long)
+                        }
+                    }
+                    .addOnFailureListener {
+                        Log.e("Location_error", "${it.message}")
+                    }
+            }
+        }
+        if (hasPermissions(context, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            locationUpdate()
+        } else {
+            askPermissions(context as Activity, REQUEST_LOCATION_PERMISSION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+        }
+
+        onDispose {}
+    }
+    return currentUserLocation
 }
 
-@RequiresPermission(
-    anyOf = [Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION],
-)
-@Composable
-fun CurrentLocationContent(usePreciseLocation: Boolean) {
-    val scope = rememberCoroutineScope()
-    val context = LocalContext.current
-    val locationClient = remember {
-        LocationServices.getFusedLocationProviderClient(context)
-    }
-    var locationInfo by remember {
-        mutableStateOf("")
-    }
-
-    Column(
-        Modifier
-            .fillMaxWidth()
-            .animateContentSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Button(
-            onClick = {
-                //To get more accurate or fresher device location use this method
-                scope.launch(Dispatchers.IO) {
-                    val priority = if (usePreciseLocation) {
-                        Priority.PRIORITY_HIGH_ACCURACY
-                    } else {
-                        Priority.PRIORITY_BALANCED_POWER_ACCURACY
-                    }
-                    val result = locationClient.getCurrentLocation(
-                        priority,
-                        CancellationTokenSource().token,
-                    ).await()
-                    result?.let { fetchedLocation ->
-                        locationInfo =
-                            "Current location is \n" + "lat : ${fetchedLocation.latitude}\n" +
-                                    "long : ${fetchedLocation.longitude}\n" + "fetched at ${System.currentTimeMillis()}"
-                    }
-                }
-            },
-        ) {
-            Text(text = "Get current location")
+fun hasPermissions(context: Context, vararg permissions: String): Boolean {
+    for (permission in permissions) {
+        if (ContextCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+            return false
         }
-        Text(
-            text = locationInfo,
-        )
     }
+    return true
+}
+
+fun askPermissions(activity: Activity, requestCode: Int, vararg permissions: String) {
+    if (!hasPermissions(activity, *permissions)) {
+        ActivityCompat.requestPermissions(activity, permissions, requestCode)
+    }
+}
+
+@SuppressLint("MissingPermission")
+fun locationUpdate() {
+    locationCallback.let {
+        val locationRequest: LocationRequest =
+            LocationRequest().apply {
+                interval = TimeUnit.SECONDS.toMillis(60)
+                fastestInterval = TimeUnit.SECONDS.toMillis(30)
+                maxWaitTime = TimeUnit.MINUTES.toMillis(2)
+                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            }
+        locationProvider.requestLocationUpdates(locationRequest, it, Looper.getMainLooper())
+    }
+}
+
+fun getReadableLocation(latitude: Double, longitude: Double, context: Context): String {
+    var addressText = ""
+    val geocoder = Geocoder(context, Locale.getDefault())
+    try {
+        val addresses = geocoder.getFromLocation(latitude, longitude, 1)
+        if (addresses?.isNotEmpty() == true) {
+            val address = addresses[0]
+            addressText = address.locality +  ", " + address.countryName
+            Log.d("geolocation", addressText)
+        }
+    } catch (e: IOException) {
+        Log.d("geolocation", e.message.toString())
+    }
+    return addressText
 }
 
 @Preview
---
+++ app/src/main/java/dev/abhinav/compass/LatAndLong.kt
@@ -0,0 +1,6 @@
+package dev.abhinav.compass
+
+data class LatAndLong(
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0
+)
